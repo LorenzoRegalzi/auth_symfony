56b936041b8232303e82be91f44e4c061cda700e
LorenzoRegalzi <lorenzo.regalzi@gmail.com>
2020-10-31 11:26:10 +0100

    auth

---
+++ src/Form/UserType.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\CallbackTransformer;
+
+
+class UserType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('email')
+            ->add('roles', ChoiceType::class, [
+                'required' => true,
+                'multiple' => false,
+                'expanded' => false,
+                'choices' => [
+                    'User' => 'ROLE_USER',
+                    'Admin' => 'ADMIN_USER',
+                ]
+            ])
+            ->add('password')
+            ->add('website')
+            ->add('details')
+        ;
+
+        $builder->get('roles')
+                ->addModelTransformer(new CallbackTransformer(
+                    function ($rolesArray) {
+                        return count($rolesArray) ? $rolesArray[0] : null;                  //transform the array in to string or in null
+                    },
+                    function ($rolesString) {
+                        return [$rolesString];                                              //transform the string back in array 
+                    }
+                ));
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
